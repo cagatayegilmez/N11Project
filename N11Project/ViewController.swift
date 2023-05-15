838538e75c49b832e00bfe6407d9689cb7405490
Çağatay Eğilmez <cagatay.egilmez@softtech.com.tr>
2023-05-15 18:17:47 +0300

    projectCreate

---
+++ N11Project/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  N11Project
+//
+//  Created by Çağatay Eğilmez on 15.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
